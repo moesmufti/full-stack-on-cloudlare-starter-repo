d67664bdaa74d7e58a202900c471b86e7e8a45f0
moes-91 <m.mufti91@gmail.com>
2025-09-04 21:34:27 +0200

    feat: enhance LinkClickTracker with offset time management and add durable query functions for click data retrieval

--- apps/data-service/src/durable-objects/link-click-tracker.ts
+++ apps/data-service/src/durable-objects/link-click-tracker.ts
@@ -1,22 +1,35 @@
+import { deleteClicksBefore, getRecentClicks } from "@/helpers/durable-queries";
 import { DurableObject } from "cloudflare:workers";
 import moment from "moment";
 
 export class LinkClickTracker extends DurableObject {
 	sql: SqlStorage;
+	mostRecentOffsetTime: number = 0;
+	leastRecentOffsetTime: number = 0;
 
 	constructor(ctx: DurableObjectState, env: Env) {
 		super(ctx, env);
 		this.sql = ctx.storage.sql;
 
 		ctx.blockConcurrencyWhile(async () => {
+			const [leastRecentOffsetTime, mostRecentOffsetTime] = await Promise.all([
+				ctx.storage.get<number>("leastRecentOffsetTime"),
+				ctx.storage.get<number>("mostRecentOffsetTime"),
+			]);
+
+			this.leastRecentOffsetTime =
+				leastRecentOffsetTime || this.leastRecentOffsetTime;
+			this.mostRecentOffsetTime =
+				mostRecentOffsetTime || this.mostRecentOffsetTime;
+
 			this.sql.exec(`
-            CREATE TABLE IF NOT EXISTS geo_link_clicks (
-                latitude REAL NOT NULL,
-                longitude REAL NOT NULL,
-                country TEXT NOT NULL,
-                time INTEGER NOT NULL
-            )
-        `);
+                CREATE TABLE IF NOT EXISTS geo_link_clicks (
+                    latitude REAL NOT NULL,
+                    longitude REAL NOT NULL,
+                    country TEXT NOT NULL,
+                    time INTEGER NOT NULL
+                )
+            `);
 		});
 	}
 
@@ -36,15 +49,56 @@ export class LinkClickTracker extends DurableObject {
 			country,
 			time,
 		);
+		const alarm = await this.ctx.storage.getAlarm();
+		if (!alarm)
+			await this.ctx.storage.setAlarm(moment().add(2, "seconds").valueOf());
+	}
+
+	async alarm() {
+		console.log("alarm");
+		const clickData = getRecentClicks(this.sql, this.mostRecentOffsetTime);
+
+		const sockets = this.ctx.getWebSockets();
+		for (const socket of sockets) {
+			socket.send(JSON.stringify(clickData.clicks));
+		}
+
+		await this.flushOffsetTimes(clickData.mostRecentTime, clickData.oldestTime);
+		await deleteClicksBefore(this.sql, clickData.oldestTime);
+	}
+
+	async flushOffsetTimes(
+		mostRecentOffsetTime: number,
+		leastRecentOffsetTime: number,
+	) {
+		this.mostRecentOffsetTime = mostRecentOffsetTime;
+		this.leastRecentOffsetTime = leastRecentOffsetTime;
+		await this.ctx.storage.put(
+			"mostRecentOffsetTime",
+			this.mostRecentOffsetTime,
+		);
+		await this.ctx.storage.put(
+			"leastRecentOffsetTime",
+			this.leastRecentOffsetTime,
+		);
 	}
 
 	async fetch(_: Request) {
-		const websocketPair = new WebSocketPair();
-		const [client, server] = Object.values(websocketPair);
+		const webSocketPair = new WebSocketPair();
+		const [client, server] = Object.values(webSocketPair);
 		this.ctx.acceptWebSocket(server);
-        return new Response(null, {
-            status: 101,
-            webSocket: client,
-        })
+		return new Response(null, {
+			status: 101,
+			webSocket: client,
+		});
+	}
+
+	webSocketClose(
+		ws: WebSocket,
+		code: number,
+		reason: string,
+		wasClean: boolean,
+	): void | Promise<void> {
+		console.log("client closed");
 	}
 }
